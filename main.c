432dcf5dce22dc6fb1ff86cf5fe65f3452b4a651
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2024-03-19 17:44:34 -0300

    .

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
-    printf("Ola mundo!");
+    int option;
+    printf("Bem vindo a pizzaria Pizzas\n Oque Desejas?\n");
+    printf("[1]Pedir uma pizza\n");
+    printf("[2]Sair\n");
+    scanf("%i",&option);
+    if(option == 1){
+        printf("Vamos escolher o tamanho da pizza!");
+    }else{
+        return 0;
+    }
     return 0;
 }
