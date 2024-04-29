bf7f339a5c46545dc1a98a9b88fb45764a572d02
LeonardoAValerio <leonardoaugustovalerio@gmail.com>
2024-04-29 18:10:26 -0300

    Switch problem

--- main.c
+++ main.c
@@ -1,130 +1,61 @@
+//Imagine como se fosse um pedido no Mc'Donalds
+//Pedido - Fazer um pedido de cada vez, considerando que um Pedido pode ter infinitas pizzas!
+//Fazer o retorno dos sabores, tamanhos, em files! que servirá pra não precisar converter!
+//Cadastro - Guardar em Files, e poder verificar se ele cadastrou certo!
+//Selecionar a porção da pizza que o sabor irá ter!
+//Nota fiscal, !!FUNCIONAL!!
+//Adicionais/Montar sua pizza
+//Registro de quantos pedidos ele já fez em todo o aplicativo! (opcional)
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
-#define SIZE_MAX_FLAVORS 4
+#define maxFlavors 4
 
 typedef struct{
-    int size;
-    int flavors[SIZE_MAX_FLAVORS];
+    char size[100];
+    int flavors[maxFlavors];
     float price;
 }pizza;
 
-int tamanhoPizza();
-int* saboresPizza();
-void pedidosPizza(pizza*, int);
-
 int main(){
-    int option, currentSize, sizeMaxArr = 0;
-    int* arrFlavor;
-    pizza* pizzaArr = malloc(sizeMaxArr * sizeof(pizza));
+    int option, quan = 0;
+    //Menu Inicial
     do{
-        system("cls");
-        printf("Bem vindo a Pizzaria Galla!\n");
-        printf("\t[2]-Novo Pedido\n");
-        printf("\t[1]-Verificar/Confirmar Pedidos\n");
-        printf("\t[0]-Sair\n");
-        printf("Digite oque deseja fazer: ");
-        scanf("%d",&option);
-        switch (option)
-        {
-        case 2:
-            sizeMaxArr++;
-            currentSize = sizeMaxArr-1;
-            pizzaArr = realloc(pizzaArr, sizeMaxArr * sizeof(pizza));
-            printf("Vamos montar uma pizza!\n");
-            pizzaArr[currentSize].size = tamanhoPizza();
-            arrFlavor = saboresPizza();
-            for(int k = 0; k<SIZE_MAX_FLAVORS; k++){
-                pizzaArr[currentSize].flavors[k] = arrFlavor[k];
-            }
-            continue;
+    system("cls");
+    printf("Bem-vindo a Pizzaria Galla!\n");
+    printf("\t[1] - Pedir uma pizza\n");
+    printf("\t[0] - Sair\n");
+    printf("Informe oque deseja fazer: ");
+    scanf("%d",&option);
+    switch (option){
+    case 1:
+        //Pedir a pizza
+        do{ //Quantas pizzas
+            printf("Informe quantas pizzas o senhor deseja: \n");
+            scanf("%d",&quan); //Cria o array de struct das pizzas
+        }while(quan>0);
+        pizza arrayPizzas[quan];
         
-        case 1:
-            pedidosPizza(pizzaArr, sizeMaxArr);
-            continue;
 
-        case 0:
-            printf("Volte Sempre!\n");
-            continue;
-                    
-        default:
-            printf("Opcao invalida!\n");
-            continue;
+        for(int i = 0; i<quan; i++){ //Loop para preencher todas as pizzas
+            //Tamanho
+            arrayPizzas[i].price = 2;
+            //Sabores
         }
+        break;
+    
+    case 0:
+        //Sair
+        printf("Volte sempre!");
+        break;
+
+    default:
+        //Opcao invalida
+        break;
+    }
     }while(option != 0);
-    free(pizzaArr);
     return 0;
 }
 
-int tamanhoPizza(){
-    int optionT = 1;
-    do{
-        printf("Escolha o tamanho da sua pizza!\n");
-        printf("\t[1] - Pequeno \n");
-        printf("\t[2] - Medio\n");
-        printf("\t[3] - Grande\n");
-        printf("\t[4] - Familia\n");
-        printf("Digite o valor do tamanho da pizza: ");
-        scanf("%d",&optionT);
-        if(optionT < 0 || optionT > 4){
-            printf("Opcao invalida!");
-            optionT = 0;
-        }
-    }while (optionT == 0);
-    return optionT;
-}
-
-int* saboresPizza(){
-    int *flavorP = malloc(SIZE_MAX_FLAVORS * sizeof(int));
-    int quan, check = 1;
-    for(int i = 0; i<SIZE_MAX_FLAVORS; i++){
-        flavorP[i] = 0;
-    }
-    do{
-        printf("Informe quantos sabores deseja(1-4): ");
-        scanf("%d",&quan);
-        if(quan<1 || quan >4){
-            printf("Opcao invalida!\n");
-            check = 0;
-            continue;
-        }
-
-        printf("CARDAPIO DE SABORES:\n");
-        printf("\t[1] - Calabresa\n");
-        printf("\t[2] - Frango com Catupiry\n");
-        printf("\t[3] - Marguerita\n");
-        printf("\t[4] - 4 Queijos\n");
-        printf("\t[5] - Tomate Seco\n");
-        printf("\t[6] - Moda da Casa\n");
-        for(int i = 0; i<quan; i++){
-            printf("Digite o valor do seu %d sabor: ",i+1);
-            scanf("%d",&flavorP[i]);
-        }
-        check = 1;
-    }while(check == 0);
-    return flavorP;
-}
-
-void pedidosPizza(pizza* arr, int sizeArr){
-    int optionP, k;
-    do{
-        printf("Seus pedidos sao:\n");
-        for(k = 0; k<sizeArr; k++){
-            printf("\t[Pizza %d]:\n",k+1);
-            printf("\tTamanho: %d\n",arr[k].size);
-            printf("\tSabores: \n");
-            for(int i = 0; i<SIZE_MAX_FLAVORS; i++){
-                int printFlavor = arr[k].flavors[i];
-                if(printFlavor != 0){
-                    printf("\t%d\n",printFlavor);
-                }
-            }  
-        }
-        if(k > 0){
-            printf("\t[2] - Confirmar Pedido(s)\n");
-            printf("\t[1] - Cancelar Pedido(s)\n");
-        }
-        printf("\t[0] - Voltar\n");
-        scanf("%d",&optionP);
-    }while(optionP);
-}
